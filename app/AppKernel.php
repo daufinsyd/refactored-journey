198cb41a4b475e656eba76824baa21539869bc34
SimVil <simon.vilmin@gmail.com>
2016-12-28 19:28:39 +0100

    solved conflicts

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,10 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-<<<<<<< HEAD
             new OC\PlatformBundle\OCPlatformBundle(),
-=======
->>>>>>> 9055a20150a59480eea4f3324846236dc0860f05
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
