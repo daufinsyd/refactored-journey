fd04c4c5a07d963121c7d6380cf88980e06bf407
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-04 22:48:52 +0100

    init + getters/setters

---
+++ src/LOG/LoginBundle/Entity/User.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sydney_manjaro
+ * Date: 04/01/17
+ * Time: 22:35
+ */
+
+namespace LOG\LoginBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table(name="log_user")
+ * @ORM\Entity(repositoryClass="LOG\LoginBundle\Entity\UserRepository")
+ */
+
+class Login
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(name="email", type"string", length=255)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(name="passwd", type="string", length=255)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    protected $passwd;
+
+    /**
+     * @ORM\Column(name="status", type="integer")
+     */
+    protected $status;
+
+    /**
+     * @ORM\Column(name="rank", type="smallint")
+     */
+    protected $rank;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="RASP\RaspBundle\Entity\Ufr")
+     */
+    protected $ufr;
+
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUfr()
+    {
+        return $this->ufr;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPasswd()
+    {
+        return $this->passwd;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRank()
+    {
+        return $this->rank;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @param mixed $passwd
+     */
+    public function setPasswd($passwd)
+    {
+        $this->passwd = $passwd;
+    }
+
+    /**
+     * @param mixed $rank
+     */
+    public function setRank($rank)
+    {
+        $this->rank = $rank;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param mixed $ufr
+     */
+    public function setUfr($ufr)
+    {
+        $this->ufr = $ufr;
+    }
+}
