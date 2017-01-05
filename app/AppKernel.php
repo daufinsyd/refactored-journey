b83c87ff812785c59c3ac8eae3c1931783f90a7e
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-05 16:11:00 +0100

    enable FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new OC\PlatformBundle\OCPlatformBundle(),
             new LOG\LoginBundle\LOGLoginBundle(),
             new RASP\RaspBundle\RASPRaspBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
