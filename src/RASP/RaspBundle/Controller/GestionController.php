5b6e38f750bcf0881e48b58a54472a3b05ed8cb6
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-07 16:01:01 +0100

    try edit user

--- src/RASP/RaspBundle/Controller/GestionController.php
+++ src/RASP/RaspBundle/Controller/GestionController.php
@@ -14,9 +14,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 // for absolute path
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
 // for JsonsResponse
 use Symfony\Component\HttpFoundation\JsonResponse;
+
+// for types
+use RASP\RaspBundle\Form\User\UserType;
 
 class GestionController extends Controller {
     // Actions for RCCF admin
@@ -30,25 +32,46 @@ class GestionController extends Controller {
         return $this->render('RASPRaspBundle:User/Gestion:users.html.twig');
     }
 
-    public function userAction($id)
+    public function userAction($user_id)
     {
         // Show specific user
-        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find(1);
+        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id);
         return $this->render("RASPRaspBundle:User/Gestion:user.html.twig", array("user" => $user));
     }
 
-    public function editUserAction($id)
+    public function editUserAction($user_id, Request $request)
     {
         // Show specific user
-        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find(1);
+        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id);
+
         $listUfr = $this->getDoctrine()->getRepository("RASPRaspBundle:Ufr")->findAll();
-        return $this->render("RASPRaspBundle:User/Gestion:editUser.html.twig", array("user" => $user, "listUfr" => $listUfr));
+
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_showUser', array('user_id' => $user_id));
+        }
+
+        return $this->render('RASPRaspBundle:User/Gestion:editUser.html.twig', array('form' => $form->createView()));
     }
 
-    public function createUser()
+    public function createUserAction()
     {
         // Create a new user
         return $this;
+    }
+
+    public function userSuccessAction($user_id) {
+        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id);
+        return $this->render("RASPRaspBundle:User/Gestion:user.html.twig", array("user" => $user));
     }
 
     public function groupAction(){
---
+++ src/RASP/RaspBundle/Form/User/UfrType.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sydney_manjaro
+ * Date: 07/01/17
+ * Time: 15:07
+ */
+
+
+namespace RASP\RaspBundle\Form\User;
+
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use RASP\RaspBundle\Entity\Ufr;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class UfrType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name');
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'RASP\RaspBundle\Entity\Ufr', //UfrType::class,
+        ));
+    }
+}
