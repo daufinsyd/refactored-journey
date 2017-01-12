722713ab341b1b8a83490573afad24b5b28ac08f
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-12 22:27:03 +0100

    create RCOM Bundle

---
+++ src/RASP/RComBundle/Controller/DefaultController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RASP\RComBundle\Controller;
+
+use RASP\RaspBundle\Entity\Raspberry;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('RComBundle:Default:index.html.twig');
+    }
+
+    public function whatsupAction(Request $request=null){
+        if ($request != null) {
+            $content = $request->getContent();
+
+            $id = $request->get('id');
+            $status = $request->get('status');
+            $info = $request->get('info');
+            $shortLog = $request->get('shortLog');
+
+            $fs = new Filesystem();
+            $data = array("id" => $id, "info" => $info, "status" => $status, "shortLog" => $shortLog);
+            $json = json_encode($data);
+            $fs->dumpFile('/home/sydney_manjaro/tmp.json', $json);
+        }
+        return new Response(200);
+    }
+}
