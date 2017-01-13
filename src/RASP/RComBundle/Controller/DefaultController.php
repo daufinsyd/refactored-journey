1e98bfd363af0276fef21e31ff12c90d1f09f284
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-13 23:19:49 +0100

    serialization for pendingActions (imbored)

--- src/RASP/RComBundle/Controller/DefaultController.php
+++ src/RASP/RComBundle/Controller/DefaultController.php
@@ -10,7 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
+
+// Serialization
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class DefaultController extends Controller
 {
@@ -38,10 +42,18 @@ class DefaultController extends Controller
 
     public function imboredAction(Request $request = null){
         if ($request != null){
+            // Get actionList
             $uuid = $request->get('uuid');
             $rasp = $this->getDoctrine()->getRepository("RASPRaspBundle:Raspberry")->findBy(array("uuid" => $uuid));
             $actionList = $this->getDoctrine()->getRepository("RComBundle:RaspAction")->findBy(array("rasp" => $rasp));
-            $json = $this->get("serializer")->serialize($actionList, 'json');
+
+            // Serialize actionList with only required fields
+            $normalizer = new ObjectNormalizer();
+            $normalizer->setIgnoredAttributes(array('ufr', 'rasp'));
+            $encoder = new JsonEncoder();
+            $serializer = new Serializer(array($normalizer), array($encoder));
+            $serializer->serialize($actionList, 'json');
+            $json = $serializer->serialize($actionList, 'json');
 
             return new Response($json);
         }
