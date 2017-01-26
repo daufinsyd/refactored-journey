df9a2d132704e6cb797d20c385cd12bf26565e5a
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-26 21:50:04 +0100

    use SimpleCalendarPlus

--- src/RASP/RaspBundle/Controller/CalendarController.php
+++ src/RASP/RaspBundle/Controller/CalendarController.php
@@ -3,6 +3,7 @@
 namespace RASP\RaspBundle\Controller;
 
 use donatj\SimpleCalendar;
+use donatj\SimpleCalendarPlus;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,8 +13,11 @@ class CalendarController extends Controller
     public function showCalendarAction($calendar_id){
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
 
-        $calendar = new SimpleCalendar();
+        $calendar = new SimpleCalendarPlus();
         $calendar->setDate("June 5 2010");
+        $calendar->addDailyHtml("Coucou :)", "June 6 2010 10:7:02");
+        $calendar->addDailyHtml("Coucou3 :)", "June 6 2010 11:8:03");
+        $calendar->addDailyHtml("Coucou2 :)", "June 7 2010 12:9:04");
 
         return $this->render('@RASPRasp/Calendar/calendar.html.twig', array('loggedInUser' => $loggedInUser, "calendar" => $calendar));
     }
