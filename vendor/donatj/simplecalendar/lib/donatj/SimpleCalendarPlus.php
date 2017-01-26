5f04413063a106d58a7469a868dce5b9dabf1619
Sydney Gems <sydney.gems@openmailbox.org>
2017-01-26 21:51:14 +0100

    create SimpleCalendarPlus.php

--- vendor/donatj/simplecalendar/lib/donatj/SimpleCalendarPlus.php
+++ vendor/donatj/simplecalendar/lib/donatj/SimpleCalendarPlus.php
@@ -3,14 +3,15 @@
 namespace donatj;
 
 /**
- * Simple Calendar
- *
+ * Simple Calendar+
+ * @author Sydney Gems <sydney.gems@openmailbox.org>
+ * Based from Simple Calendar
  * @author Jesse G. Donat <donatj@gmail.com>
  * @link http://donatstudios.com
  * @license http://opensource.org/licenses/mit-license.php
  *
  */
-class SimpleCalendar {
+class SimpleCalendarPlus {
 
 	/**
 	 * Array of Week Day Names
@@ -19,9 +20,10 @@ class SimpleCalendar {
 	 */
 	public $wday_names = false;
 
+    private $nexEventId = 0;  // nextEvent ID
 	private $now = false;
 	private $daily_html = array();
-    // $this->daily_html[$this->now['year']][$this->now['mon']][6][0]  donne txt souhaité
+    // $this->daily_html[year][day][hour][minute] give array(id, html)
 	private $offset = 0;
 
 	/**
@@ -55,7 +57,6 @@ class SimpleCalendar {
 	 * @param null|string $end_date_string Date string for when the event ends. Defaults to start date
 	 */
 	public function addDailyHtml( $html, $start_date_string, $end_date_string = null ) {
-		static $htmlCount = 0;
 		$start_date = strtotime($start_date_string);
 		if( $end_date_string ) {
 			$end_date = strtotime($end_date_string);
@@ -63,14 +64,15 @@ class SimpleCalendar {
 			$end_date = $start_date;
 		}
 
+		// Add event for each day between start_date and end_date (with the same hour and same minute)
 		$working_date = $start_date;
 		do {
 			$tDate = getdate($working_date);
 			$working_date += 86400;
-			$this->daily_html[$tDate['year']][$tDate['mon']][$tDate['mday']][$htmlCount] = $html;
+			$this->daily_html[$tDate['year']][$tDate['mon']][$tDate['mday']][$tDate['hours']][$tDate['minutes']] = array($this->nexEventId, $html);
 		} while( $working_date < $end_date + 1 );
 
-		$htmlCount++;
+        $this->nexEventId++;  // define id for the next event
 
 	}
 
@@ -136,15 +138,24 @@ class SimpleCalendar {
 
 			$out .= '<time datetime="' . date('Y-m-d', $datetime) . '">' . $i . '</time>';
 
+            // Get a day
 			$dHtml_arr = false;
 			if( isset($this->daily_html[$this->now['year']][$this->now['mon']][$i]) ) {
 				$dHtml_arr = $this->daily_html[$this->now['year']][$this->now['mon']][$i];
 			}
 
+			// Append events
 			if( is_array($dHtml_arr) ) {
 				foreach( $dHtml_arr as $dHtml ) {
-					$out .= '<div class="event" name="e">' . $dHtml . '</div>';
-				}
+                    for ($j = 0; $j < 59; $j++) {
+                        if(array_key_exists($j, $dHtml)) {
+                            /*
+                             * Display the event with the html value and store the ID of the event in the name element of the div element
+                             */
+                            $out .= '<div class="event" name="'.$dHtml[$j][0].'">' . $dHtml[$j][1] . '</div>';
+                        }
+                    }
+                }
 			}
 
 			$out .= "</td>";
