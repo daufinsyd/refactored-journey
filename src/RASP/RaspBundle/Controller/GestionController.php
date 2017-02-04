ced879672713fe2e0b9822afc4c9f3dccb735aaf
SimVil <simon.vilmin@gmail.com>
2017-02-04 01:46:35 +0100

    added documentaion

--- src/RASP/RaspBundle/Controller/GestionController.php
+++ src/RASP/RaspBundle/Controller/GestionController.php
@@ -30,27 +30,81 @@ use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class GestionController extends Controller {
-    // Actions for RCCF admin for users
+// envoyer lien de validation par mail
 
+/* class GestionController ---------------------------------------------------------------------------------------------
+ * Attributes :
+ *
+ * Methods :
+ *     public usersAction()
+ *     public userAction(int)  (beware of the difference user / users)
+ *     public function userPasswordAction(int, Request)
+ *     public function editUserAction(int, Request)
+ *     public function createUserAction(Request)
+ *     public function deleteUserAction(int)
+ *     public function userSuccessAction(int)
+ *     public function toggleUserAction(int)
+ *
+ * Description :
+ *     Aims to handle user gesture, that is creation/display/deletion/update and so forth.
+ *
+--------------------------------------------------------------------------------------------------------------------- */
+
+class GestionController extends Controller {
+
+
+    /* usersAction -------------------------------------------------------------------------------------------------
+     * Input :
+     * Output :
+     *   Redirection to a listing page.
+     *
+     * Desc :
+     *   display the list of all known users with information such as their status, name, and so on.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function usersAction(){
-        // List users
-        $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
+        $loggedInUser = $this->get('security.token_storage')->getToken()->getUser(); // get current user
         $listUser = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->findAll();
         return $this->render('RASPRaspBundle:User/Gestion:users.html.twig', array("listUser" => $listUser, 'loggedInUser' => $loggedInUser));
     }
 
+
+
+    /* userAction ------------------------------------------------------------------------------------------------------
+     * Input :
+     *   int $user_id --> id of an user we would like to have infos on.
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Aims to reroute on a specific user page. Depending on the access of the logged user, the function will
+     *   either redirect  on the required page, or throw an AccessDeniedException exception.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function userAction($user_id)
     {
-        // Show specific user
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
-        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id);
+        $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id); // find user in database through Doctrine
+
+        // if user is granted with super_admin status, then he can access the required page
         if ($user->getId() == $loggedInUser->getId() || $this->isGranted('ROLE_ADMIN')) {
             return $this->render("RASPRaspBundle:User/Gestion:user.html.twig", array("user" => $user, 'loggedInUser' => $loggedInUser));
+
+        } else {
+            throw new AccessDeniedException("Vous n'avez pas les bonnes permissions");
         }
-        else throw new AccessDeniedException("Vous n'avez pas les bonnes permissions");
     }
 
+
+    /* userPasswordAction ----------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     *   int     $user_id --> the id the request refers to
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Helps to change an user password. If data are valid, the new pass is submitted, reroute to user gesture
+     *   otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function userPasswordAction($user_id, Request $request)
     {
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
@@ -59,8 +113,9 @@ class GestionController extends Controller {
         $form = $this->createForm(UserPasswdType::class, $user);
         $form->handleRequest($request);
 
+        // if the submitted form is valid
         if($form->isSubmitted() && $form->isValid()) {
-            //$user = $form->getData();
+            // get the new pass and update
             $user->setPlainPassword($form->get('password')->getData());
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
@@ -71,6 +126,18 @@ class GestionController extends Controller {
         return $this->render('RASPRaspBundle:User/Gestion:userPassword.html.twig', array('form' => $form->createView(), 'user' => $user, 'loggedInUser' => $loggedInUser));
     }
 
+
+    /* editUserdAction -------------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     *   int     $user_id --> the id the request refers to
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Helps to change an user password. If data are valid, the new pass is submitted, reroute to user gesture
+     *   otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function editUserAction($user_id, Request $request)
     {
         // Show specific user
@@ -98,6 +165,17 @@ class GestionController extends Controller {
         else throw new AccessDeniedException("Vous n'avez pas les bonnes permissions.");
     }
 
+
+    /* createUfrAction -------------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Tries to create a new ufr. Form is valid implies redirection to
+     *   ufr main page, to ufr edition page otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function createUserAction(Request $request)
     {
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
@@ -124,6 +202,17 @@ class GestionController extends Controller {
         else throw new AccessDeniedException("Vous n'avez pas les bonnes permissions.");
     }
 
+
+    /* createUfrAction -------------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Tries to create a new ufr. Form is valid implies redirection to
+     *   ufr main page, to ufr edition page otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function deleteUserAction($user_id)
     {
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
@@ -141,12 +230,34 @@ class GestionController extends Controller {
 
     }
 
+
+    /* createUfrAction -------------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Tries to create a new ufr. Form is valid implies redirection to
+     *   ufr main page, to ufr edition page otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function userSuccessAction($user_id) {
         $loggedInUser = $this->get('security.token_storage')->getToken()->getUser();
         $user = $this->getDoctrine()->getRepository("RASPRaspBundle:User")->find($user_id);
         return $this->render("RASPRaspBundle:User/Gestion:user.html.twig", array("user" => $user, 'loggedInUser' => $loggedInUser));
     }
 
+
+    /* createUfrAction -------------------------------------------------------------------------------------------------
+     * Input :
+     *   Request $request --> result got from page (e.g a form)
+     * Output :
+     *   Redirection to a page depending on input.
+     *
+     * Desc :
+     *   Tries to create a new ufr. Form is valid implies redirection to
+     *   ufr main page, to ufr edition page otherwise.
+     * -------------------------------------------------------------------------------------------------------------- */
     public function toggleUserAction($user_id)
     {
         // TODO: implement ROLE
